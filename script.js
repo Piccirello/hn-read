873cb7c21ea9017b6249450b648ca4cf238a524b
Tom Piccirello <Piccirello@users.noreply.github.com>
2018-12-10 10:59:30 -0500

    Remove use of GM_ functions

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // ==UserScript==
 // @name         Hacker News visited highlighter
 // @namespace    https://piccirello.com/
-// @version      0.1
-// @description  Highlights visited links for better visibility
+// @version      0.2
+// @description  Highlights visited post links for better visibility
 // @author       Tom Piccirello
 // @match        https://news.ycombinator.com/*
-// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    GM_addStyle (".storylink:visited { color: #ff6600; }");
-    GM_addStyle (".subtext>a:nth-of-type(3):visited { color: #ff6600; }");
+    var addStyle = function (css) {
+      var style = document.createElement('style');
+      style.textContent = css;
+      document.documentElement.appendChild(style);
+      // return style; //optional, but convenient for changing the styling later.
+    };
+
+    addStyle (".storylink:visited { color: #ff6600; }");
+    addStyle (".subtext>a:nth-of-type(3):visited { color: #ff6600; }");
 })();
